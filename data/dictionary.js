5de81a93e6d64381516da4c9f4afab2c624be422
Yang <kevin.chen01@gymboglobal.com>
2019-11-01 15:48:47 +0800

    照片重新整合排序

--- data/dictionary.js
+++ data/dictionary.js
@@ -2,28 +2,10 @@ window.worldObj = {
     "0Asian": {
         "urls": [{
             "type": "image",
-            "url": "../data/3worldwishes/0Asian/00Tailand|100|13.5/image/重庆|永川|娅娅.jpg"
-        }, {
-            "type": "image",
-            "url": "../data/3worldwishes/0Asian/00Tailand|100|13.5/image/重庆|永川|卿陈一家.jpg"
-        }, {
-            "type": "image",
-            "url": "../data/3worldwishes/0Asian/00Tailand|100|13.5/image/重庆|川外|陈倩2.jpg"
+            "url": "../data/3worldwishes/0Asian/05QingHai|100|37/image/江苏|常州|杨宇.jpg"
         },{
             "type": "image",
-            "url": "../data/3worldwishes/0Asian/01Indian|80|23/image/湖北|武汉|彭金秀.jpg"
-        }, {
-            "type": "image",
-            "url": "../data/3worldwishes/0Asian/02ShanXi|112|38/image/山东|威海|杨老二.jpg"
-        }, {
-            "type": "image",
-            "url": "../data/3worldwishes/0Asian/03HK|114|22/image/上海|浦东|小葫芦.jpg"
-        }, {
-            "type": "image",
             "url": "../data/3worldwishes/0Asian/05QingHai|100|37/image/上海|宝山|ace一家.jpg"
-        }, {
-            "type": "image",
-            "url": "../data/3worldwishes/0Asian/06BeiJin|116|40/image/上海|浦东|冰糖.jpg"
         }, {
             "type": "image",
             "url": "../data/3worldwishes/0Asian/07TaiWan|121|26/image/广东|暨南大学|陈倩同学.jpg"
@@ -47,7 +29,7 @@ window.worldObj = {
             "url": "../data/3worldwishes/0Asian/09Sichuan|102|32/image/重庆|重庆大学|陈倩同学4.jpg"
         },{
             "type": "image",
-            "url": "../data/3worldwishes/0Asian/09Sichuan|102|32/image/重庆|川外|陈倩同学5.jpg"
+            "url": "../data/3worldwishes/0Asian/09Sichuan|102|32/image/重庆|西南大学|陈倩同学5.jpg"
         }],
         "urlJson": "../data/3worldwishes/0Asian/info.json"
     },
@@ -71,21 +53,36 @@ window.worldObj = {
     "3Africa": {
         "urls": [{
             "type": "image",
-            "url": "../data/3worldwishes/3Africa/1GoodHope|18|-34/image/小萌|好望角|飞鸟.jpg"
+            "url": "../data/3worldwishes/3Africa/1GoodHope|18|-34/image/重庆|沙坪坝|唐代武.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/3Africa/1GoodHope|18|-34/image/重庆|九龙坡|周鹏.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/3Africa/1GoodHope|18|-34/image/重庆|南坪|黄飞内.jpg"
         }],
         "urlJson": "../data/3worldwishes/3Africa/info.json"
     },
     "4Europe": {
         "urls": [{
             "type": "image",
-            "url": "../data/3worldwishes/4Europe/1Rome|12|41/image/溦露鱼|罗马.jpg"
+            "url": "../data/3worldwishes/4Europe/1Rome|12|41/image/四川|成都|胡天银一家.png"
         }],
         "urlJson": "../data/3worldwishes/4Europe/info.json"
     },
     "5Oceania": {
         "urls": [{
             "type": "image",
+            "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/湖北|武汉|彭金秀.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/江西|吉安|程雨欣.jpg"
+        },{
+            "type": "image",
             "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/贵州|贵阳|邓向尊.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/山东|威海|杨老二.jpg"
         },{
             "type": "image",
             "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/山东|威海|杨冰.jpg"
@@ -95,16 +92,53 @@ window.worldObj = {
         },{
             "type": "image",
             "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/云南|昆明理工|张博.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/广东|深圳|邱伟杰.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/浙江|温州|陈大龙.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/江西|南昌|刁嘉兴.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/5Oceania/1Australia|151|31/image/西藏|布达拉宫|胡其旭.png"
         }],
         "urlJson": "../data/3worldwishes/5Oceania/info.json"
     },
     "6Asian": {
-        "urls": [ {
+        "urls": [{
             "type": "image",
-            "url": "../data/3worldwishes/6Asian/2Hangzhou|120|30/image/杭州|阿里巴巴|马云.jpg"
+            "url": "../data/3worldwishes/6Asian/2Hangzhou|120|30/image/上海|浦东|小葫芦.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/6Asian/2Hangzhou|120|30/image/上海|浦东|小冰糖.jpg"
         }],
         "urlJson": "../data/3worldwishes/6Asian/info.json"
+    },
+    "7chongqing": {
+        "urls": [{
+            "type": "image",
+            "url": "../data/3worldwishes/7chongqing/00yongchuan|100|13.5/image/广东|深圳|灿灿.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/7chongqing/00yongchuan|100|13.5/image/重庆|川外|陈倩.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/7chongqing/00yongchuan|100|13.5/image/重庆|永川|杨行.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/7chongqing/00yongchuan|100|13.5/image/山东|潍坊学院|卿悦.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/7chongqing/00yongchuan|100|13.5/image/重庆|永川|杨娅.jpg"
+        },{
+            "type": "image",
+            "url": "../data/3worldwishes/7chongqing/00yongchuan|100|13.5/image/重庆|永川|卿陈一家.jpg"
+        }],
+        "urlJson": "../data/3worldwishes/7chongqing/info.json"
     }
 }
-window.worldArr =["0Asian","1NorthAmerica","2SouthAmerican","5Oceania"]
-// window.worldArr =["0Asian","1NorthAmerica","2SouthAmerican","3Africa","4Europe","5Oceania","6Asian"]
+// window.worldArr =["3Africa","0Asian","1NorthAmerica","2SouthAmerican","4Europe","5Oceania"]
+window.worldArr =["0Asian","1NorthAmerica","2SouthAmerican","3Africa","4Europe","5Oceania","6Asian","7chongqing"]
